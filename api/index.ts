c6ee5473f3b75d170bd38169299043f4014126a9
jbp3 <john.b.petersen@gmail.com>
2025-10-30 12:39:10 -0400

    fixing it

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 // api/index.ts
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import { app as agentApp } from "../src/agent";
+import { app as agentApp } from "../src/agent.js";
 
 const app = new Hono().route("/", agentApp as any);
 
